f50857a6dff2b53d3cfc4b37fad8e2690e688098
ManelSln <92730566+ManelSln@users.noreply.github.com>
2021-10-18 14:38:51 +0200

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -1,4 +1,4 @@
-print("hello")
+
 from numpy import *
 from sklearn import *
 import sklearn.preprocessing as sp #pour iris 
@@ -6,3 +6,8 @@ import sklearn.preprocessing as sp #pour iris
 iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
+
+def PPV(X,Y):
+    R= 0,8*X.shape[0] #Nombre de données reference
+    T =0,2*X.shape[0] #Nombre de données test
+    
