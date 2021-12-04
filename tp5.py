89dd3733767ee3e78ed719c0055d285f1610d922
yokotsuno12 <92730680+yokotsuno12@users.noreply.github.com>
2021-12-04 12:58:19 +0100

    Update tp5.py

--- tp5.py
+++ tp5.py
@@ -1,3 +1,11 @@
+# TP5 - SDN
+
+#### Ce TP présente ce qu’est une Descente de Gradient (DG). La DG s’applique lorsquel’on cherche le minimum d’une fonction dont on connaît l’expression analytique, qui estdérivable, mais dont le calcul direct du minimum est difficile. C’est un algorithme fondamental à connaître car utilisé partout sous des formes dérivées. Nous n’étudions ici que la version de base.
+
+## Descente de gradient
+
+#### Ce paragraphe présente la DG sur la minimisation de la fonction E(x)quelconque. Le problème est de trouver la valeur de x qui minimise E(x). Pour trouver analytiquement le minimum de la fonction E, il faut trouver les racines de l’équation E′(x) = 0, donc trouver ici les racines d’un polynôme de degré 3, ce qui est des fois “difficile". Donc on va utiliser la DG. La DG consiste à construire une suite de valeurs xi(avec x0 fixé au hasard) de manière itérative : xi+1=xi−ηE′(xi).On peut donner un critère de fin à la DG par exemple si xi+1−xi< epsilon ou si i > nombremax. Pour ce problème, nous utilisons epsilon = 0.01 et nombremax = 1000.
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -61,6 +69,25 @@ DG_c = DG_E(5, 0.1)
 DG_d = DG_E(5, 0.17)
 #DG_e = DG_E(5, 1)
 DG_f = DG_E(0, 0.001)
+visualisation = [DG_a, DG_b, DG_c, DG_d, DG_f]
+
+#Autre manière pour les visualiser toutes d'un coup ;) 
+i=0
+plt.figure()
+plt.figsize = (20,20)
+for j in visualisation :
+    i+=1
+    plt.subplot(1,5,i)
+    plt.scatter(list(range(len(j))), j, color='blue')
+    plt.plot(list(range(len(j))), j, color='red')
+    plt.xlabel("epoch")
+    plt.ylabel("x trouvé")
+plt.subplots_adjust(left=0.1, 
+                    bottom=0.1,  
+                    right=3.5,  
+                    top=1,  
+                    wspace=0.4,  
+                    hspace=0.4)
 
 plt.plot(list(range(len(DG_a))), DG_a, color='blue')
 plt.title("x0 = 5 et nu = 0.001")
@@ -94,3 +121,8 @@ plt.title("x0 = 0 et nu = 0.001")
 plt.xlabel("epoch")
 plt.ylabel("x trouvé")
 plt.show()
+
+
+
+# DESCENTE DE GRADIENT POUR LA REGRESSION LINEAIRE
+
